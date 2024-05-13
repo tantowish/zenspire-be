7387e300ee3463715ae805c7ec8a19be8de4b4e5
tanto <tantows001@gmail.com>
2024-05-13 20:03:50 +0700

    fix: discussion like seed include

--- prisma/seeders/seed.ts
+++ prisma/seeders/seed.ts
@@ -1,6 +1,7 @@
 import { prismaClient } from "../../src/app/database";
 import { comments } from "./comment-seeder";
 import { discussions } from "./discussion-seeder";
+import { discussionLikes } from "./discussionLike-seeder";
 import { journals } from "./journal-seeder";
 import { users } from "./user-seeder";
 import { userData } from "./userData-seeder";
@@ -24,6 +25,10 @@ async function main() {
 
     await prismaClient.comment.createMany({
         data: comments
+    })
+
+    await prismaClient.discussionLike.createMany({
+        data: discussionLikes
     })
 }
 
