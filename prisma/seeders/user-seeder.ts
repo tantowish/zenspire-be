ce4cdada9432ef4816f915d3b16e411b55565063
tanto <tantows001@gmail.com>
2024-05-17 21:28:53 +0700

    fix: user password seeder

--- prisma/seeders/user-seeder.ts
+++ prisma/seeders/user-seeder.ts
@@ -1,17 +1,17 @@
 export const users = [
     {
         "first_name": "user 1",
-        "password": "password",
+        "password": "$2b$10$SjvwcT2kvs3U0.p4ph57s.Ov2/n2fsgFMeNB8XFb7VQfw3u2cQwmW",
         "email": "user1@gmail.com"
     },
     {
         "first_name": "user 2",
-        "password": "password",
+        "password": "$2b$10$SjvwcT2kvs3U0.p4ph57s.Ov2/n2fsgFMeNB8XFb7VQfw3u2cQwmW",
         "email": "user2@gmail.com"
     },
     {
         "first_name": "user 3",
-        "password": "password",
+        "password": "$2b$10$SjvwcT2kvs3U0.p4ph57s.Ov2/n2fsgFMeNB8XFb7VQfw3u2cQwmW",
         "email": "user3@gmail.com"
     },
 ]
